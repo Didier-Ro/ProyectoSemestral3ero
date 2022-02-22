e11f89f2d33dead496ec98e86d2f8e0cd8139c7d
Didier-Ro <ortizrojeroh@gmail.com>
2022-02-21 21:54:21 -0600

    Movimiento

--- Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
+++ Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private bool _thrusting = default;
-    private float _turnDirection = default;
+    private Rigidbody2D _rgb2d;
+
+    private bool _thrusting;
+    private float _turnDirection;
+
+    public float playerSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    private void Awake()
+    {
+        _rgb2d = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
-        _thrusting = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 
-        if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow)){
+        if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow)){
             _turnDirection = 1.0f;
         }else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
             _turnDirection = -1.0f;
         }else{
             _turnDirection = 0;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_thrusting){
+            _rgb2d.AddForce(this.transform.up * this.playerSpeed);
+        }
+
+        if (_turnDirection != 0.0f){
+            _rgb2d.AddTorque(_turnDirection * this.turnSpeed);
         }
     }
 }
