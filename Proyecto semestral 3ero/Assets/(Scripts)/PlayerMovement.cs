dac55991b6df102c3c52146832f732ca775b36f4
Didier-Ro <ortizrojeroh@gmail.com>
2022-02-22 07:54:02 -0600

    Player Request Resolve

--- Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
+++ Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody2D;
+    private Rigidbody2D _rigidbody2D = default;
 
     private bool _thrusting = default;
     private float _turnDirection = default;
