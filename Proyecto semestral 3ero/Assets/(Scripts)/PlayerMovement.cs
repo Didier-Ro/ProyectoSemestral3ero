d382d3715a6a9b19b40a64c7a67de417c12a6bab
Didier-Ro <ortizrojeroh@gmail.com>
2022-02-22 07:32:31 -0600

    Pull Request Resolve

--- Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
+++ Proyecto semestral 3ero/Assets/(Scripts)/PlayerMovement.cs
@@ -1,41 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _rgb2d;
+    private Rigidbody2D _rigidbody2D;
 
-    private bool _thrusting;
-    private float _turnDirection;
+    private bool _thrusting = default;
+    private float _turnDirection = default;
 
-    public float playerSpeed = 1.0f;
-    public float turnSpeed = 1.0f;
+    public float _playerSpeed = 1.0f;
+    public float _turnSpeed = 1.0f;
 
     private void Awake()
     {
-        _rgb2d = GetComponent<Rigidbody2D>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
     private void Update()
     {
         _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 
-        if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow)){
+        if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow))
+        {
             _turnDirection = 1.0f;
-        }else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
             _turnDirection = -1.0f;
-        }else{
+        }
+        else
+        {
             _turnDirection = 0;
         }
     }
+
     private void FixedUpdate()
     {
-        if (_thrusting){
-            _rgb2d.AddForce(this.transform.up * this.playerSpeed);
+        if (_thrusting)
+        {
+            _rigidbody2D.AddForce(transform.up * _playerSpeed);
         }
 
-        if (_turnDirection != 0.0f){
-            _rgb2d.AddTorque(_turnDirection * this.turnSpeed);
+        if (_turnDirection != 0.0f)
+        {
+            _rigidbody2D.AddTorque(_turnDirection * _turnSpeed);
         }
     }
+
 }
